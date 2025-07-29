c4b7f0d7d5f479e4e6f628cbabd5293e96749d9a
mohseni-mr <mohamad.r.mohseni@gmail.com>
2025-07-29 08:51:40 +0100

    Update build scripts and add new setup executable for MM Image Optimizer

--- compile-nuitka.bat
+++ compile-nuitka.bat
@@ -1,3 +1,3 @@
 @echo off
-nuitka --onefile --windows-console-mode=disable --enable-plugins=pyside6 --windows-icon-from-ico=mohseni.ico --output-dir=build --remove-output main.py -o MMImageOptimizer.exe
+nuitka --onefile --windows-console-mode=disable --enable-plugins=pyside6 --windows-icon-from-ico=mohseni.ico --output-dir=dist --remove-output main.py -o MMImageOptimizer.exe
 pause
